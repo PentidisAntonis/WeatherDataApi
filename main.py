2c0b2ee57bc509c0e5da047c37421c14e5b2593b
ahpen <a.h.pentidis@gmail.com>
2024-01-12 11:47:50 +0200

    showing all dates for a station

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ stations = stations[["STAID", "STANAME                                 "]]
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
-#
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    # if not for orient="records" it brakes the data and they look awful
+    return result
 
 
 @app.route("/api/v1/<station>/<date>")
