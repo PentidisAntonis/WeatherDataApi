8f56600bbe7e32d23284765eafd303d9722d67c1
ahpen <a.h.pentidis@gmail.com>
2024-01-12 11:06:34 +0200

    adding the stations on the home page

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import pandas as pd
 
 app = Flask("Website")
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+# adding the stations on the home page so that user can see all the possible stations
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations)
 
 
 @app.route("/api/v1/<station>/<date>")
